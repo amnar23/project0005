22bd13a75588ea6177787ad6cdd0a041441ec770
Amna <amnar23498@gmail.com>
2020-09-01 16:15:30 +0500

    Turn ON/OFF

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function Room() {
   return <div className={`room ${brightness}`}>
     <h2>The room is {brightness}</h2><br/>
     <button className='button-design' onClick={()=>{setLit(!isLit);setCount(count+1);}}>FLIP</button>
+    <br/>
+    <button onClick={()=>setLit(true)}>Turn ON</button>
+    <button onClick={()=>setLit(false)}>Turn OFF</button>
     <p>Button is clicked {count} times</p>
   </div>
 }
